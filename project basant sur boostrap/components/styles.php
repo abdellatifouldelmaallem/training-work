bfef8b980a27f6b302cbe77fc1bc1309782f9008
abdellatif <ouldeabdellatif40@gmail.com>
2021-02-16 09:27:10 +0100

    this folder has all you need

---
+++ project basant sur boostrap/components/styles.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+
+<!-- This file should be at the end -->
+<link rel="stylesheet" href="./css/style.css">
---
+++ project basant sur boostrap/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Agency</title>
+    <?php include './components/styles.php'; ?>
+</head>
+<body>
+
+    <?php include './components/header.php'; ?>
+    <?php include './components/services.php'; ?>
+    <?php include './components/about.php'; ?>
+    <?php include './components/realisation.php'; ?>
+    <?php include './components/team.php'; ?>
+    <?php include './components/clients.php'; ?>
+    <?php include './components/testimonials.php'; ?>
+    <?php include './components/pricing.php'; ?>
+    <?php include './components/footer.php'; ?>
+    <?php include './components/scripts.php';?>
+</body>
+
+</html>
