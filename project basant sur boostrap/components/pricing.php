23713fdb6ca3a81f83cf94d28a656cb19a3060e0
unknown <moumou.souhaila@gmail.com>
2021-02-17 10:16:04 +0100

    adding section testimonial & pricing

---
+++ project basant sur boostrap/components/pricing.php
@@ -0,0 +1,66 @@
+<!------ pricing ------>
+<section id="pricing" class="pricing py-5">
+      <div class="box-content">
+         <div class="title-content">
+            <h3 class="animateme animated fadeInUp">Pricing</h3>
+         </div>
+         <div class="underline animated fadeInDown"></div>
+            <div class="container">
+                <div class="row">
+      <!-- BASIC Tier -->
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">basic</h5>
+            <h6 class="card-price text-center">120 DH<span class="period">/month</span></h6>
+            <hr>
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Item One</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Item Two</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Item Tree</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Item Four</li>
+
+            </ul>
+            <a href="#" class="btn btn-block btn-info text-uppercase">get</a>
+          </div>
+        </div>
+      </div>
+      <!-- STARTER Tier -->
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">starter</h5>
+            <h6 class="card-price text-center">240 DH<span class="period">/month</span></h6>
+            <hr>
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Item One</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Item Two</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Item Tree</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Item Four</li>
+
+            </ul>
+            <a href="#" class="btn btn-block btn-info text-uppercase">get</a>
+          </div>
+        </div>
+      </div>
+      <!-- ENTREPRISE Tier -->
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">entreprise</h5>
+            <h6 class="card-price text-center">480 DH<span class="period">/month</span></h6>
+            <hr>
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Item One</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Item Two</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Item Tree</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Item Four</li>
+
+            </ul>
+            <a href="#" class="btn btn-block btn-info text-uppercase">get</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ project basant sur boostrap/components/testimonials.php
@@ -0,0 +1,55 @@
+<!------ Témoignage ------>
+<section id="Testimonals">
+      <div id="cover-temoin" class="bg-paralax">
+         <div class="box-content">
+            <div class="title-content title-content-white">
+               <h3 class="animateme animated fadeInUp">Témoignage</h3>
+            </div>
+<div class="container px-2 px-md-4 py-5 mx-auto ">
+    <div class="row d-flex justify-content-center ">
+        <div class="col-lg-5 col-md-7 col-sm-9 ">
+            <div id="carouselExampleIndicators" class="carousel slide d-flex" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item">
+                        <div class="card border-0 ">
+                            <div class="card-header bg-info"></div>
+                            <div class="card-body text-center "><img class="img-1 img-fluid" src="https://i.imgur.com/nUNhspp.jpg">
+                                <div class="quotes"><img class="quotes-img card-title" src="https://pngimage.net/wp-content/uploads/2019/05/icon-tbt-png-.png" />
+                                    <p class="card-text bold text-center px-md-3 intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.</p>
+                                    <h6 class="font-weight-bold mt-4">Nicholas</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item active">
+                        <div class="card border-0 ">
+                            <div class="card-header bg-info"></div>
+                            <div class="card-body text-center "><img class="img-1 img-fluid" src="https://heroshotphotography.com/wp-content/uploads/2018/03/Headshot-Smiling-Chinese.jpg">
+                                <div class="quotes"><img class="quotes-img card-title" src="https://pngimage.net/wp-content/uploads/2019/05/icon-tbt-png-.png" />
+                                    <p class="card-text bold text-center px-md-3 intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.</p>
+                                    <h6 class="font-weight-bold mt-4">Sophia</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="card border-0 ">
+                            <div class="card-header bg-info"></div>
+                            <div class="card-body text-center "><img class="img-1 img-fluid" src="https://i.imgur.com/At1IG6H.png">
+                                <div class="quotes"><img class="quotes-img card-title" src="https://pngimage.net/wp-content/uploads/2019/05/icon-tbt-png-.png" />
+                                    <p class="card-text bold text-center px-md-3 intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.</p>
+                                    <h6 class="font-weight-bold mt-4">Mike</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
