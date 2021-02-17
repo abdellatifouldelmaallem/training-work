8443a8a46511390dcffe9d5abb938e6fac2c3b76
unknown <houda.ghazi199@gmail.com>
2021-02-17 13:37:28 +0100

    modification en footer

---
+++ project basant sur boostrap/components/footer.php
@@ -0,0 +1,121 @@
+<section class="al-stats">
+  <div class="container">
+    <h2 class="never text-center">Never stop learning</h2><br><br>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="al-stat-box text-center">
+          <div class="container"> 
+            <a href="#">
+              <span class="glyphicon  text-info glyphicon-download"></span>
+            </a>
+          </div>
+          <div class="al-stat-meta">
+            <h3 class="text-info">1.6B</h3>
+            <span>DOWNLOADS</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="al-stat-box text-center">
+          <div class="container"> 
+            <a href="#">
+              <span class="glyphicon text-info fas fa-award"></span>
+            </a>  
+          </div>                  
+          <div class="al-stat-meta">
+            <h3 class="text-info">3.2B</h3>
+            <span>AWARDS</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="al-stat-box text-center">
+          <div class="container"> 
+            <a href="#">
+              <span class="glyphicon text-info far fa-thumbs-up"></span>
+            </a>
+          </div>                    
+          <div class="al-stat-meta">
+            <h3 class="text-info">99M</h3>
+            <span>LIKES</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="al-stat-box text-center">
+          <div class="container"> 
+            <a href="#">
+              <span class="glyphicon text-info far fas fa-thumbs-up"></span>
+            </a>   
+          </div>                 
+          <div class="al-stat-meta">
+            <h3 class="text-info">200B</h3>
+            <span>RECOMANDED</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<footer class="border-dark">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        <h2 class="font-weight-bold display-3 text-uppercase mt-3 mb-4 text-white">YCA</h2>
+        <p class="lorem text-white" >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br> <br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <ul class="list-unstyled list-inline text-center">
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/" class="btn-floating btn-lg btn-fb" type="button" role="button"><i class="fab fa-facebook-f text-info"></i></a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://twitter.com/" class="btn-floating btn-lg btn-tw" type="button" role="button"><i class="fab fa-twitter text-info"></i></a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.youtube.com/" class="btn-floating btn-lg btn-yt" type="button" role="button"><i class="fab fa-youtube text-info"></i></a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.google.com/" class="btn-floating btn-lg btn-gplus" type="button" role="button"><i class="fab fa-google-plus-g text-info"></i></a>
+            </li>
+          </ul>
+      </div>
+      <div class="col-sm">
+        <div id="contact-right">
+          <form class="formulaire">
+            <h3 class="title text-white"> Contactez nous</h3>            
+            <!-- Small input -->
+            <input class="form-control form-control-sm" type="text" placeholder="Full name"><br>
+            <!-- Small input -->
+            <input class="form-control" type="text" placeholder="Email adresse"> <br>           
+            <!-- Large input -->
+            <textarea class="form-control" rows="9 id="comment" value="Message">Message </textarea> <br>
+            <button type="submit" class="btn btn-info">ENVOYER</button>
+            <button type="submit" class="btn btn-info" id="stop">STOP</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<div>
+  <div class="row row-cols-auto">
+    <div class="col">
+      <div class="footer-copyright text-center py-3 font-weight-bold">© 2020 Copyright:
+        <a href="https://mdbootstrap.com/" class="text-info font-weight-bold"> MDBootstrap.com</a>
+      </div>
+    </div>
+    <div class="col">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item "><a href="#" class="text-info">Accueil</a></li>
+          <li class="breadcrumb-item"><a href="#" class="text-info">Services</a></li>
+          <li class="breadcrumb-item active" aria-current="page"><a href="#" class="text-info">Réalisations</a> </li>      
+          <li class="breadcrumb-item"><a href="#" class="text-info">Equipes</a></li>
+          <li class="breadcrumb-item"><a href="#" class="text-info">Clients</a></li>
+          <li class="breadcrumb-item"><a href="#" class="text-info">Testimonials</a></li>
+          <li class="breadcrumb-item"><a href="#" class="text-info">Pricing</a></li>
+        </ol>
+      </nav>
+    </div>
+  </div>
+</div>
+        
--- project basant sur boostrap/index.php
+++ project basant sur boostrap/index.php
@@ -4,6 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,500,600,900" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+    <script>
+        jQuery(document).ready(function($) {
+            $('.counter').counterUp({
+                delay: 10,
+                time: 1000
+            });
+        });
+    </script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
     <title>Web Agency</title>
     <?php include './components/styles.php'; ?>
 </head>
@@ -19,6 +35,12 @@
     <?php include './components/pricing.php'; ?>
     <?php include './components/footer.php'; ?>
     <?php include './components/scripts.php';?>
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 </body>
 
 </html>
