183503e9a809f3103f1750b3c910773728beafd9
unknown <houda.ghazi199@gmail.com>
2021-02-17 13:01:39 +0100

    add footer

--- project basant sur boostrap/components/footer.php
+++ project basant sur boostrap/components/footer.php
@@ -6,11 +6,11 @@
         <div class="al-stat-box text-center">
           <div class="container"> 
             <a href="#">
-              <span class="glyphicon glyphicon-download"></span>
+              <span class="glyphicon  text-info glyphicon-download"></span>
             </a>
           </div>
           <div class="al-stat-meta">
-            <h3>1.6B</h3>
+            <h3 class="text-info">1.6B</h3>
             <span>DOWNLOADS</span>
           </div>
         </div>
@@ -19,11 +19,11 @@
         <div class="al-stat-box text-center">
           <div class="container"> 
             <a href="#">
-              <span class="glyphicon fas fa-award"></span>
+              <span class="glyphicon text-info fas fa-award"></span>
             </a>  
           </div>                  
           <div class="al-stat-meta">
-            <h3>3.2B</h3>
+            <h3 class="text-info">3.2B</h3>
             <span>AWARDS</span>
           </div>
         </div>
@@ -32,11 +32,11 @@
         <div class="al-stat-box text-center">
           <div class="container"> 
             <a href="#">
-              <span class="glyphicon far fa-thumbs-up"></span>
+              <span class="glyphicon text-info far fa-thumbs-up"></span>
             </a>
           </div>                    
           <div class="al-stat-meta">
-            <h3>99M</h3>
+            <h3 class="text-info">99M</h3>
             <span>LIKES</span>
           </div>
         </div>
@@ -45,11 +45,11 @@
         <div class="al-stat-box text-center">
           <div class="container"> 
             <a href="#">
-              <span class="glyphicon far fas fa-thumbs-up"></span>
+              <span class="glyphicon text-info far fas fa-thumbs-up"></span>
             </a>   
           </div>                 
           <div class="al-stat-meta">
-            <h3>200B</h3>
+            <h3 class="text-info">200B</h3>
             <span>RECOMANDED</span>
           </div>
         </div>
@@ -61,20 +61,20 @@
   <div class="container">
     <div class="row">
       <div class="col-sm">
-        <h3 class="font-weight-bold text-uppercase mt-3 mb-4 text-white">YCA</h3>
+        <h2 class="font-weight-bold display-3 text-uppercase mt-3 mb-4 text-white">YCA</h2>
         <p class="lorem text-white" >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br> <br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
           <ul class="list-unstyled list-inline text-center">
             <li class="list-inline-item">
-              <a href="https://www.facebook.com/" class="btn-floating btn-lg btn-fb" type="button" role="button"><i class="fab fa-facebook-f"></i></a>
+              <a href="https://www.facebook.com/" class="btn-floating btn-lg btn-fb" type="button" role="button"><i class="fab fa-facebook-f text-info"></i></a>
             </li>
             <li class="list-inline-item">
-              <a href="https://twitter.com/" class="btn-floating btn-lg btn-tw" type="button" role="button"><i class="fab fa-twitter"></i></a>
+              <a href="https://twitter.com/" class="btn-floating btn-lg btn-tw" type="button" role="button"><i class="fab fa-twitter text-info"></i></a>
             </li>
             <li class="list-inline-item">
-              <a href="https://www.youtube.com/" class="btn-floating btn-lg btn-yt" type="button" role="button"><i class="fab fa-youtube"></i></a>
+              <a href="https://www.youtube.com/" class="btn-floating btn-lg btn-yt" type="button" role="button"><i class="fab fa-youtube text-info"></i></a>
             </li>
             <li class="list-inline-item">
-              <a href="https://www.google.com/" class="btn-floating btn-lg btn-gplus" type="button" role="button"><i class="fab fa-google-plus-g"></i></a>
+              <a href="https://www.google.com/" class="btn-floating btn-lg btn-gplus" type="button" role="button"><i class="fab fa-google-plus-g text-info"></i></a>
             </li>
           </ul>
       </div>
@@ -87,9 +87,9 @@
             <!-- Small input -->
             <input class="form-control" type="text" placeholder="Email adresse"> <br>           
             <!-- Large input -->
-            <textarea class="form-control" rows="5" id="comment" value="Message">Message </textarea> <br>
-            <button type="submit" class="btn btn-primary">ENVOYER</button>
-            <button type="submit" class="btn btn-primary" id="stop">STOP</button>
+            <textarea class="form-control" rows="9 id="comment" value="Message">Message </textarea> <br>
+            <button type="submit" class="btn btn-info">ENVOYER</button>
+            <button type="submit" class="btn btn-info" id="stop">STOP</button>
           </form>
         </div>
       </div>
@@ -99,20 +99,20 @@
 <div>
   <div class="row row-cols-auto">
     <div class="col">
-      <div class="footer-copyright text-center py-3">© 2020 Copyright:
-        <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+      <div class="footer-copyright text-center py-3 font-weight-bold">© 2020 Copyright:
+        <a href="https://mdbootstrap.com/" class="text-info font-weight-bold"> MDBootstrap.com</a>
       </div>
     </div>
     <div class="col">
       <nav class="navbar navbar-expand-lg navbar-light">
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="#">Accueil</a></li>
-          <li class="breadcrumb-item"><a href="#">Services</a></li>
-          <li class="breadcrumb-item active" aria-current="page"><a href="#">Réalisations</a> </li>      
-          <li class="breadcrumb-item"><a href="#">Equipes</a></li>
-          <li class="breadcrumb-item"><a href="#">Clients</a></li>
-          <li class="breadcrumb-item"><a href="#">Testimonials</a></li>
-          <li class="breadcrumb-item"><a href="#">Pricing</a></li>
+          <li class="breadcrumb-item "><a href="#" class="text-info">Accueil</a></li>
+          <li class="breadcrumb-item"><a href="#" class="text-info">Services</a></li>
+          <li class="breadcrumb-item active" aria-current="page"><a href="#" class="text-info">Réalisations</a> </li>      
+          <li class="breadcrumb-item"><a href="#" class="text-info">Equipes</a></li>
+          <li class="breadcrumb-item"><a href="#" class="text-info">Clients</a></li>
+          <li class="breadcrumb-item"><a href="#" class="text-info">Testimonials</a></li>
+          <li class="breadcrumb-item"><a href="#" class="text-info">Pricing</a></li>
         </ol>
       </nav>
     </div>
