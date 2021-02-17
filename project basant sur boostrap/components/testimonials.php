d194283f8b5595bb66dcac02b9a644bb0465f2d9
unknown <moumou.souhaila@gmail.com>
2021-02-17 11:14:43 +0100

    fixing card-header

--- project basant sur boostrap/components/testimonials.php
+++ project basant sur boostrap/components/testimonials.php
@@ -17,7 +17,7 @@
                 <div class="carousel-inner">
                     <div class="carousel-item">
                         <div class="card border-0 ">
-                            <div class="card-header bg-info"></div>
+                            <div class="card-header bg-info height-120"></div>
                             <div class="card-body text-center "><img class="img-1 img-fluid" src="https://i.imgur.com/nUNhspp.jpg">
                                 <div class="quotes"><img class="quotes-img card-title" src="https://pngimage.net/wp-content/uploads/2019/05/icon-tbt-png-.png" />
                                     <p class="card-text bold text-center px-md-3 intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.</p>
@@ -28,7 +28,7 @@
                     </div>
                     <div class="carousel-item active">
                         <div class="card border-0 ">
-                            <div class="card-header bg-info"></div>
+                            <div class="card-header bg-info height-120"></div>
                             <div class="card-body text-center "><img class="img-1 img-fluid" src="https://heroshotphotography.com/wp-content/uploads/2018/03/Headshot-Smiling-Chinese.jpg">
                                 <div class="quotes"><img class="quotes-img card-title" src="https://pngimage.net/wp-content/uploads/2019/05/icon-tbt-png-.png" />
                                     <p class="card-text bold text-center px-md-3 intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.</p>
@@ -39,7 +39,7 @@
                     </div>
                     <div class="carousel-item">
                         <div class="card border-0 ">
-                            <div class="card-header bg-info"></div>
+                            <div class="card-header bg-info height-120"></div>
                             <div class="card-body text-center "><img class="img-1 img-fluid" src="https://i.imgur.com/At1IG6H.png">
                                 <div class="quotes"><img class="quotes-img card-title" src="https://pngimage.net/wp-content/uploads/2019/05/icon-tbt-png-.png" />
                                     <p class="card-text bold text-center px-md-3 intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.</p>
