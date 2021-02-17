6b4ca0ad243cb3f95dc303b2810804cb96ce7087
Med-dahreddine <dahreddine.med2@gmail.com>
2021-02-17 11:25:17 +0100

    adding my section

---
+++ project basant sur boostrap/components/services.php
@@ -0,0 +1,95 @@
+<section id="mohamed">
+<div class="box-content">
+    <div class="title-content">
+        <h3 class="underline">Services</h3>
+
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <!-- first row -->
+            <div class=" col-lg-4 col-md-6 col-xd-12 row">
+                <div class="col-md-2">
+                    <i class="bi bi-pencil-fill"></i>
+                </div>
+                <div class="col-md-10">
+                    <h4>Webdesign<h4>
+                    <p class="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maiores tempora ullam veniam illo, modi unde.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-xd-12 row">
+                <div class="col-md-2">
+                    <i class="bi bi-layout-text-window"></i>
+                </div>
+                <div class="col-md-10">
+                    <h4>Développement web<h4>
+                    <p class="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maiores tempora ullam veniam illo, modi unde.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-xd-12 row">
+                <div class="col-md-2">
+                    <i class="bi bi-phone-fill"></i>
+                </div>
+                <div class="col-md-10">
+                    <h4>Application mobile<h4>
+                    <p class="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maiores tempora ullam veniam illo, modi unde.</p>
+                </div>
+            </div>
+            
+            <!-- first columns -->
+            <div class="col-lg-4 col-md-6 col-xd-12 row">
+                <div class="col-md-2"> 
+                    <i class="bi bi-bullseye"></i>
+                </div>
+                <div class="col-md-10">
+                    <h4>SEO<h4>
+                    <p class="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maiores tempora ullam veniam illo, modi unde.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-xd-12 row">
+                <div class="col-md-2">
+                    <i class="bi bi-palette2"></i>
+                </div>
+                <div class="col-md-10">
+                    <h4>UX/UI<h4>
+                    <p class="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maiores tempora ullam veniam illo, modi unde.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-xd-12 row">
+                <div class="col-md-2">
+                    <i class="bi bi-sliders"></i>
+                </div>
+                <div class="col-md-10">
+                    <h4>Maintenance<h4>
+                    <p class="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maiores tempora ullam veniam illo, modi unde.</p>
+                </div>
+            </div>
+            
+
+        </div>
+    
+    
+    </div>
+    <div class="container">
+        <div class="row">
+            <!-- Qui sommes nous -->
+            <div class="col-md-4 text-center">
+                
+                <h3 class="nous">Qui sommes nous</h3>
+                <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate id odio enim nisi maiores at molestias voluptas magni voluptatum. Dolorum saepe, autem minima reiciendis laudantium qui. Expedita architecto doloribus ab? Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae consequuntur eveniet aperiam aliquid illum ipsum cupiditate odit inventore necessitatibus molestiae qui commodi totam, reprehenderit mollitia laborum facilis tempore maxime eaque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur architecto quas fuga porro sequi, a animi .</p>
+                <button  type="button" class="btn btn-primary text-center btn-marge">Nos Réalisations</button>
+                
+            </div>
+            <!-- image -->
+            <div class="col-md-8 background"></div>
+        </div>
+    </div>
+
+
+</div>
+
+</section>
