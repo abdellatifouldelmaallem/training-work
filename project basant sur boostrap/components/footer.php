d335a33c9b3cc5371797c83e0f325324952e8556
unknown <houda.ghazi199@gmail.com>
2021-02-17 11:35:06 +0100

    add section footer

---
+++ project basant sur boostrap/components/footer.php
@@ -0,0 +1,121 @@
+<section class="al-stats">
+  <div class="container">
+    <h2 class="never text-center">Never stop learning</h2><br><br>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="al-stat-box text-center">
+          <div class="container"> 
+            <a href="#">
+              <span class="glyphicon glyphicon-download"></span>
+            </a>
+          </div>
+          <div class="al-stat-meta">
+            <h3>1.6B</h3>
+            <span>DOWNLOADS</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="al-stat-box text-center">
+          <div class="container"> 
+            <a href="#">
+              <span class="glyphicon fas fa-award"></span>
+            </a>  
+          </div>                  
+          <div class="al-stat-meta">
+            <h3>3.2B</h3>
+            <span>AWARDS</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="al-stat-box text-center">
+          <div class="container"> 
+            <a href="#">
+              <span class="glyphicon far fa-thumbs-up"></span>
+            </a>
+          </div>                    
+          <div class="al-stat-meta">
+            <h3>99M</h3>
+            <span>LIKES</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="al-stat-box text-center">
+          <div class="container"> 
+            <a href="#">
+              <span class="glyphicon far fas fa-thumbs-up"></span>
+            </a>   
+          </div>                 
+          <div class="al-stat-meta">
+            <h3>200B</h3>
+            <span>RECOMANDED</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<footer class="border-dark">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        <h3 class="font-weight-bold text-uppercase mt-3 mb-4 text-white">YCA</h3>
+        <p class="lorem text-white" >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br> <br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <ul class="list-unstyled list-inline text-center">
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/" class="btn-floating btn-lg btn-fb" type="button" role="button"><i class="fab fa-facebook-f"></i></a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://twitter.com/" class="btn-floating btn-lg btn-tw" type="button" role="button"><i class="fab fa-twitter"></i></a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.youtube.com/" class="btn-floating btn-lg btn-yt" type="button" role="button"><i class="fab fa-youtube"></i></a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.google.com/" class="btn-floating btn-lg btn-gplus" type="button" role="button"><i class="fab fa-google-plus-g"></i></a>
+            </li>
+          </ul>
+      </div>
+      <div class="col-sm">
+        <div id="contact-right">
+          <form class="formulaire">
+            <h3 class="title text-white"> Contactez nous</h3>            
+            <!-- Small input -->
+            <input class="form-control form-control-sm" type="text" placeholder="Full name"><br>
+            <!-- Small input -->
+            <input class="form-control" type="text" placeholder="Email adresse"> <br>           
+            <!-- Large input -->
+            <textarea class="form-control" rows="5" id="comment" value="Message">Message </textarea> <br>
+            <button type="submit" class="btn btn-primary">ENVOYER</button>
+            <button type="submit" class="btn btn-primary" id="stop">STOP</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<div>
+  <div class="row row-cols-auto">
+    <div class="col">
+      <div class="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+      </div>
+    </div>
+    <div class="col">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="#">Accueil</a></li>
+          <li class="breadcrumb-item"><a href="#">Services</a></li>
+          <li class="breadcrumb-item active" aria-current="page"><a href="#">Réalisations</a> </li>      
+          <li class="breadcrumb-item"><a href="#">Equipes</a></li>
+          <li class="breadcrumb-item"><a href="#">Clients</a></li>
+          <li class="breadcrumb-item"><a href="#">Testimonials</a></li>
+          <li class="breadcrumb-item"><a href="#">Pricing</a></li>
+        </ol>
+      </nav>
+    </div>
+  </div>
+</div>
+        
